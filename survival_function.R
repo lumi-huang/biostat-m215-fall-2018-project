2c91e7683bd9c335b9c2f5dc0261b3aae693aa97
ljw9510 <ljw9510@gmail.com>
2018-12-07 14:04:07 -0800

    single cox model

--- survival_function.R
+++ survival_function.R
@@ -56,3 +56,33 @@ KMcurve = function(time, event, covariate, conf=TRUE, main=NULL) {
 KMcurve(time=breast_final$SRV_TIME_MON, event=breast_final$delta, breast_final$stage, conf = FALSE)
 legend("bottomleft", c("Stage 0", "Stage 1","Stage 2","Stage 3","Stage 4"), lty = 1:5, col = 1:5, cex=0.9)
 
+
+
+##---------------------
+# To test significance of single variable using Cox model
+
+single.cox = function(time, event, covariate, co.type) {
+  
+  p = c()
+  
+  for (i in 1:ncol(covariate)){
+    surv.sub = Surv(time=time, event=event, type="right")
+    if (co.type[i]=="N") { # continuous variable
+      formul = paste("surv.sub ~ covariate[,", i, "]", sep="")
+      fit = coxph(eval(parse(text=formul)), ties = "breslow")
+    } else { # categorical variable
+      formul = paste("surv.sub ~ factor(covariate[,", i, "])", sep="")
+      fit = coxph(eval(parse(text=formul)), ties = "breslow")
+    }
+    p = rbind(p, t(round(summary(fit)$logtest, 3))) # LRT
+  }
+  
+  return(p)
+  
+}
+
+##---------------------
+
+# Example
+co.type = c(rep("C", 4), "N") # "C" means categorical variable / "N" means numeric variable
+single.cox(breast2$SRV_TIME_MON, breast2$STAT_REC, X[,1:5], co.type)
